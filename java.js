12ae076324c4790e06faddbef46d391f77310dd2
Felix Pinson <felixpinson@etu.unicaen.fr>
2024-09-24 12:49:38 +0200

    timer update

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 let timer = document.getElementById("timer")
 
-let travail = 24
+let travail = 10
 let pause = 4
 let sec = 60
 let bool = true
@@ -24,18 +24,32 @@ function rebour(){
             travail = travail -1
             sec = 60
         }
-        timer.textContent = "Temps Restant : " + travail + " : " + sec
+        if(travail < 10){
+            if(sec < 10){
+                timer.textContent = "Temps Restant : 0" + travail + " : 0" + sec
+            }
+            timer.textContent = "Temps Restant : 0" + travail + " : " + sec
+        }else{
+            timer.textContent = "Temps Restant : " + travail + " : " + sec
+        }
+        
         if(travail === -1){
             bool = false
             pause = 4
         }
     }
     if(bool === false){
+        
         if(sec === 0){
             pause = pause -1
             sec = 60
         }
-        timer.textContent = "Temps Restant : " + pause + " : " + sec
+        if(sec < 10){
+            timer.textContent = "Temps Restant : 0" + pause + " : 0" + sec
+        }else{
+            timer.textContent = "Temps Restant : 0" + pause + " : " + sec
+        }
+        timer.textContent = "Temps Restant : 0" + pause + " : " + sec
         if(pause === -1){
             bool = true
             travail = 24
@@ -47,8 +61,17 @@ let minuteur
 let lance = document.getElementById("start")
 lance.addEventListener("click", evt=> {
     if(minuteur == null){
-        minuteur = setInterval(rebour,10)
+        minuteur = setInterval(rebour,100)
     }
+    if(bool === true){
+        buttonpause.style.display = "none"
+        buttontravail.style.display = "none"
+    }
+    if(bool === false){
+        buttontravail.style.display = "none"
+        buttonpause.style.display = "none"
+    }
+    
 }) 
 let relance = document.getElementById("restart")
 relance.addEventListener("click", evt=> {
