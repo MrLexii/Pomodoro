c5722cb4dacab0c74498ec3a9f1e4b813db3c7f2
Felix Pinson <felixpinson@etu.unicaen.fr>
2024-09-26 11:58:13 +0200

    progressBar V1

--- java.js
+++ java.js
@@ -1,9 +1,14 @@
 let timer = document.getElementById("timer")
 
-let travail = 10
+let travail = 24
 let pause = 4
 let sec = 60
 let bool = true
+
+
+const circularProgress = document.querySelectorAll("#bar");
+
+
 
 let buttontravail = document.getElementById("travail")
 buttontravail.addEventListener("click", evt=>{
@@ -27,10 +32,16 @@ function rebour(){
         if(travail < 10){
             if(sec < 10){
                 timer.textContent = "Temps Restant : 0" + travail + " : 0" + sec
+            }else{
+                timer.textContent = "Temps Restant : 0" + travail + " : " + sec
             }
-            timer.textContent = "Temps Restant : 0" + travail + " : " + sec
         }else{
-            timer.textContent = "Temps Restant : " + travail + " : " + sec
+            if(sec < 10){
+                timer.textContent = "Temps Restant : " + travail + " : 0" + sec
+            }
+            else{
+                timer.textContent = "Temps Restant : " + travail + " : " + sec
+            }
         }
         
         if(travail === -1){
@@ -49,7 +60,6 @@ function rebour(){
         }else{
             timer.textContent = "Temps Restant : 0" + pause + " : " + sec
         }
-        timer.textContent = "Temps Restant : 0" + pause + " : " + sec
         if(pause === -1){
             bool = true
             travail = 24
@@ -61,7 +71,60 @@ let minuteur
 let lance = document.getElementById("start")
 lance.addEventListener("click", evt=> {
     if(minuteur == null){
-        minuteur = setInterval(rebour,100)
+        minuteur = setInterval(rebour,1000)
+        if(bool === true){
+            Array.from(circularProgress).forEach((progressBar) => {
+                const progressValue = progressBar.querySelector("#percentage")
+                const innerCircle = progressBar.querySelector("#cercle")
+                let startValue = 0,
+                  endValue = Number(progressBar.getAttribute("value")),
+                  progressColor = progressBar.getAttribute("data-progress-color")
+              
+                const progress = setInterval(() => {
+                  startValue++
+                  progressValue.textContent = `${startValue}%`
+                  progressValue.style.color = `${progressColor}`
+              
+                  innerCircle.style.backgroundColor = `${progressBar.getAttribute(
+                    "data-inner-circle-color"
+                  )}`
+              
+                  progressBar.style.background = `conic-gradient(${progressColor} ${
+                    startValue * 3.6
+                  }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`
+                  if (startValue === endValue) {
+                    clearInterval(progress)
+                  }
+                }, 15000)
+              })
+        }
+        if(bool === false){
+            Array.from(circularProgress).forEach((progressBar) => {
+                const progressValue = progressBar.querySelector("#percentage")
+                const innerCircle = progressBar.querySelector("#cercle")
+                let startValue = 0,
+                  endValue = Number(progressBar.getAttribute("value")),
+                  progressColor = progressBar.getAttribute("data-progress-color")
+              
+                const progress = setInterval(() => {
+                  startValue++
+                  progressValue.textContent = `${startValue}%`
+                  progressValue.style.color = `${progressColor}`
+              
+                  innerCircle.style.backgroundColor = `${progressBar.getAttribute(
+                    "data-inner-circle-color"
+                  )}`
+              
+                  progressBar.style.background = `conic-gradient(${progressColor} ${
+                    startValue * 3.6
+                  }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`
+                  if (startValue === endValue) {
+                    clearInterval(progress)
+                  }
+                }, 3000)
+              })
+        }
+        
     }
     if(bool === true){
         buttonpause.style.display = "none"
